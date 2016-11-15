520177f9b2fbff380a5034c18c27988574ec4c77
Omid <omid.mogharian@dreamlines.de>
2016-11-15 15:29:45 +0100

    New: init the Interactor class

---
+++ interctor.js
@@ -0,0 +1,31 @@
+
+'use strict';
+
+class Interactor {
+
+    constructor(context = {}) {
+        if (this.constructor === Interactor) throw TypeError("No instantiation allowed, it's an abstract class");
+        this.context = context
+    }
+
+    invoke() {
+        try {
+            const fnList = [this.before, this.call, this.after];
+            const interable = fn_list.filter((e, i, arr) => { return e != undefined });
+            //todo: check as exception
+            return interable.reduce((p, fn) => p.then(fn), Promise.resolve())
+        }
+        catch (e) {
+            if (this.rollback) { this.rollback() }
+            else { throw e; }
+        }
+
+    };
+
+    //before() {}
+    call() { throw Error("Not Implimented") }
+    //after() { }
+    //rollback() { }
+}
+
+module.exports = Interactor;
